bbfcf116659fd159cef3b3fe557cd980c1a4b656
ADITYA MISHRA <adi.mishra.ayush1234@gmail.com>
2026-08-20 12:10:40 +0530

    Time: 115 ms (5.17%) | Memory: 275.5 MB (6.32%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> t;  
+    int lcs(string s1, string s2, int n, int m)
+    {
+        for(int i = 0; i < n + 1; i++)
+            for(int j = 0; j < m + 1; j++)
+                if(i == 0 || j == 0)
+                    t[i][j] = 0;
+        for(int i = 1; i < n + 1; i++)
+        {
+            for(int j = 1; j < m + 1; j++)
+            { 
+                if(s1[i - 1] == s2[j - 1])
+                    t[i][j] = 1 + t[i - 1][j - 1];
+                else
+                    t[i][j] = max(t[i - 1][j], t[i][j - 1]); 
+            }
+        } 
+        return t[n][m];      
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+         t = vector<vector<int>>(1001, vector<int>(1001, 0));
+        return lcs(text1, text2, text1.size(), text2.size());
+    }
+};
