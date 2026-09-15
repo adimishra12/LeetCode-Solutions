62d12d5de50485108b40cb0f88795e271ea71af4
ADITYA MISHRA <adi.mishra.ayush1234@gmail.com>
2026-09-16 00:41:18 +0530

    Time: 71 ms (12.42%) | Memory: 10.2 MB (13.08%) - LeetSync

--- 2031-egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.cpp
+++ 2031-egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.cpp
@@ -4,14 +4,34 @@ public:
     {
         if(f == 0 || f == 1)
             return f;
+
         if(e == 1)
             return f;
+
         if(t[e][f] != -1)
             return t[e][f];
+
         int mini = INT_MAX;
+
         for(int k = 1; k <= f; k++)
-        {
-            int temp = 1 + max(solve(e - 1, k - 1, t), solve(e, f - k, t));
+        {   int low = 0; int high = 0;
+
+            if(t[e - 1][k - 1] != -1)
+                low = t[e - 1][k - 1];
+            else
+            {
+                low = solve(e - 1, k - 1, t);
+                t[e - 1][k - 1] = low;    
+            }
+
+            if(t[e][f - k] != -1)
+                high = t[e][f - k];
+            else
+            {
+                high = solve(e, f - k, t);
+                t[e][f - k] = high;
+            }    
+            int temp = 1 + max(low, high);
             mini = min(mini, temp);
         }  
         return t[e][f] = mini;      
